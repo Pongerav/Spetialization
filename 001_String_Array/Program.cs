ef92936424fe797e7e6423c75726f1e2e975a394
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-10-15 10:11:01 +0300

    Tested array inserting

---
+++ 001_String_Array/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите количество элементов в массиве");
+
+int length = int.Parse(Console.ReadLine() ?? "0");
+string[] massiv = new string[length];
+int count = 0;
+
+Console.WriteLine("Введите элементы массива на каждой строчке");
+for (int i = 0; i < length; i += 1)
+{
+    Console.Write($"{i+1} элемент: ");
+    massiv[i] = Console.ReadLine() ?? "";
+    if (massiv[i].Length <= 3) count += 1;
+}
